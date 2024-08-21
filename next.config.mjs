84d9febe207561b2d5f1883fce5934d40b40a419
Sambid10 <sambidshk10@gmail.com>
2024-08-21 22:28:05 +0545

    initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -11,7 +11,10 @@ const nextConfig = {
 				pathname:`/a/${process.env.NEXT_PUBLIC_UPLOADTHING_APP_ID}`
 			}
 		]
-	}
+	},
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
 };
 
 export default nextConfig;
